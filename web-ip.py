4091df3b1905cae52b5448a9e7a5ab4779ea812e
Hack4peace <85699568+hack4peace@users.noreply.github.com>
2021-07-28 18:00:24 +0530

    Update web-ip.py

--- web-ip.py
+++ web-ip.py
@@ -1,3 +1,11 @@
+ import colorama
+from colorama import Fore, Style
+print(Fore.RED + '''   _             _        
+      __      __  ___ | |__         (_) _ __  
+      \ \ /\ / / / _ \| '_ \  _____ | || '_ \ 
+       \ V  V / |  __/| |_) ||_____|| || |_) |
+        \_/\_/   \___||_.__/        |_|| .__/ 
+                                    |_|    ''')
 import socket
 
 hostname = input("Enter website address: ")
